45e65a74118117676756ffc19b397aaf27b9a287
Rocael <rocaellc42@gmail.com>
2025-02-13 23:10:17 -0600

    firtsS

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     var envelope = $("#envelope");
     var btn_open = $("#open");
     var btn_reset = $("#reset");
+    var btn_flower = $("#flower");
   
     envelope.click(function () {
       open();
@@ -12,6 +13,9 @@ $(document).ready(function () {
     btn_reset.click(function () {
       close();
     });
+    btn_flower.click(function () {
+      createSunflower();
+    });
   
     function open() {
       envelope.addClass("open").removeClass("close");
@@ -19,5 +23,25 @@ $(document).ready(function () {
     function close() {
       envelope.addClass("close").removeClass("open");
     }
+    
+    function createSunflower() {
+        var flower = $('<div class="sunflower"></div>');
+        for (let i = 0; i < 12; i++) {
+            flower.append('<div class="petal"></div>');
+        }
+        flower.append('<div class="center"></div>');
+        $('body').append(flower);
+    }
+  });
+  $(document).ready(function() {
+    // Mostrar la imagen cuando se hace clic en el botón "Mirame"
+    $('#flower').click(function() {
+      $('#image-container').removeClass('hidden').fadeIn();
+    });
+  
+    // Opción para cerrar la imagen
+    $('#reset').click(function() {
+      $('#image-container').fadeOut().addClass('hidden');
+    });
   });
   
